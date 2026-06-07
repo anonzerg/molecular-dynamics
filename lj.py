be25c0cac0c7e3c5a6faf2ddf6225a4692c472db
zerg <anonzerg@proton.me>
2026-06-07 13:17:29 +0330

    fix a few typos and bugs.

--- lj.py
+++ lj.py
@@ -6,7 +6,6 @@ from numba import njit
 
 @njit(cache=True)
 def lennard_jones_potential(r, sigma, epsilon):
-    r > 0 || raise ValueError('r can not be zero.')
     c  = 4.0 * epsilon
     r6 = (sigma / r) ** 6
 
@@ -15,7 +14,6 @@ def lennard_jones_potential(r, sigma, epsilon):
 
 @njit(cache=True)
 def lennard_jones_force(r, sigma, epsilon):
-    r > 0 || raise ValueError('r can not be zero.')
     c  = 48.0 * epsilon
     r6 = (sigma / r) ** 6
 
@@ -29,19 +27,19 @@ def compute(pos, sigma, epsilon, cutoff, box_length):
     potential       = 0.0
 
     for i in range(number_of_atoms):
-        for j in range(i + 1, number_of_atoms)
-        r_vec  = pos[j] - pos[i]
-        r_vec -= np.round(pos / box_length) * box_length
-        r_mag  = np.linalg.norm(pos)
+        for j in range(i + 1, number_of_atoms):
+            r_vec  = pos[j] - pos[i]
+            r_vec -= np.round(r_vec / box_length) * box_length
+            r_mag  = np.linalg.norm(r_vec)
 
-        if 0 < r_mag < cutoff:
-            force_mag  = lennard_jones_force(r_mag, sigma, epsilon)
-            force_vec  = (r_vec / r_mag) * force_mag
+            if sigma * 0.5 < r_mag < cutoff * sigma:
+                force_mag  = lennard_jones_force(r_mag, sigma, epsilon)
+                force_vec  = (r_vec / r_mag) * force_mag
 
-            forces[i] -= force_vec
-            forces[j] += force_vec
+                forces[i] -= force_vec
+                forces[j] += force_vec
 
-            potential += lennard_jones_potential(r_mag, sigma, epsilon)
+                potential += lennard_jones_potential(r_mag, sigma, epsilon)
 
     return forces, potential
 
--- simulation.py
+++ simulation.py
@@ -2,6 +2,8 @@ import tomllib
 
 import numpy as np
 
+import lj
+
 class Config:
     def __init__(self, path):
         with open(path, 'rb') as f:
@@ -12,28 +14,31 @@ class Config:
         self.epsilon         = conf['atom']['epsilon']
         self.mass            = conf['atom']['mass']
         self.symbol          = conf['atom']['symbol']
+        self.cutoff          = conf['atom']['cutoff']
         self.number_of_atoms = conf['simulation']['number_of_atoms']
         self.steps           = conf['simulation']['steps']
         self.dt              = conf['simulation']['dt']
-        self.box_length      = conf['simulation']['box_length']
+        self.box_len         = conf['simulation']['box_length']
         self.temperature     = conf['simulation']['temperature']
-        self.density         = conf['simulation']['box_length']
+        self.density         = conf['simulation']['density']
 
 
 def init_positions(number_of_atoms, box_length):
-    points_per_dim = int(math.ceil(number_of_atoms ** 0.5))
+    points_per_dim = int(np.ceil(number_of_atoms ** (1/3)))
 
-    positions = np.zeros(number_of_atoms, 2)
+    positions = np.zeros((number_of_atoms, 3))
     spacing = box_length / points_per_dim
 
     i = 0
     for x in range(points_per_dim):
         for y in range(points_per_dim):
-            positions[i] = [
-                (x * spacing) + (spacing / 2.0),
-                (y * spacing) + (spacing / 2.0),
-                (z * spacing) + (spacing / 2.0)
-            ]
+            for z in range(points_per_dim):
+                positions[i] = [
+                    (x * spacing) + (spacing / 2.0),
+                    (y * spacing) + (spacing / 2.0),
+                    (z * spacing) + (spacing / 2.0)
+                ]
+                i += 1
         
     # make sure center of mass is the same as center of our geometric box
     offset = np.mean(positions, axis=0) - (box_length / 2.0)
@@ -43,13 +48,13 @@ def init_positions(number_of_atoms, box_length):
 
 def init_velocities(number_of_atoms, temperature, seed=42):
     rng = np.random.default_rng(seed)
-    velocities = np.random.normal(rng, np.sqrt(temperature), (number_of_atoms, 2))
+    velocities = rng.normal(0.0, np.sqrt(temperature), (number_of_atoms, 3))
 
     # this removes the COM motion
     velocities -= np.mean(velocities, axis=0)
 
     # remove 3 degree of freedom because we removed the COM motion
-    degrees_of_freedom = 3 * (number_of_atoms - 1)
+    degrees_of_freedom = 3.0 * (number_of_atoms - 1.0)
     scaler = degrees_of_freedom * temperature / np.sum(velocities ** 2)
     velocities *= scaler
     return velocities
@@ -60,7 +65,7 @@ def kinetic(velocities, mass=1.0):
 
 
 def temperature(kinetic, number_of_atoms):
-    degrees_of_freedom = 3.0 * (number_of_atoms - 1)
+    degrees_of_freedom = 3.0 * (number_of_atoms - 1.0)
     return 2 * kinetic / degrees_of_freedom * boltzmann_const
 
 
@@ -74,12 +79,12 @@ def entropy(velocities, bins=20):
 
 class Simulation:
     def __init__(self, config):
-        self.config = config
+        self.conf = config
 
     def setup(self):
-        self.pos = init_positions(self.config.number_of_atoms, self.config.box_length)
-        self.vel = init_velocities(self.config.number_of_atoms, self.config.temperature)
-        self.acc = init_accelerations()
+        self.pos = init_positions(self.conf.number_of_atoms, self.conf.box_len)
+        self.vel = init_velocities(self.conf.number_of_atoms, self.conf.temperature)
+        self.acc, _ = lj.compute(self.pos, self.conf.sigma, self.conf.epsilon, self.conf.cutoff, self.conf.box_len)
 
     def state(self):
         pass
